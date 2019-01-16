0c10dd76d908a164a9602c4228f9b8866aa956c9
Your Name <you@example.com>
2019-01-16 17:12:43 -0300

    agregando funcion filtrar

--- src/data.js
+++ src/data.js
@@ -1,8 +1,10 @@
 // esta es una función de ejemplo
 // puedes ver como agregamos la función a nuestro objeto global window
 
-const example = () => {
-  return 'example';
-};
-
-window.example = example;
+const filterFeedlabel = (data, condition) => {
+  //filter es un ciclo iterativo como el for, forech, reviw
+  const filteredFeedlabel= data.filter(element => {
+      return element.feedlabel === condition
+  })
+  return filteredFeedlabel;
+}
