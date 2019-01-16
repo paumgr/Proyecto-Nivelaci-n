5500880e87579b97f0042b4a98a4553a829282f1
Your Name <you@example.com>
2019-01-16 17:12:10 -0300

    Declarando constante para filtrar

--- main.js
+++ main.js
@@ -1,24 +1,24 @@
 
-const data = STEAM.appnews.newsitems;
+const data = (STEAM.appnews.newsitems);
 const containerRoot = document.getElementById("root");
-const selectTitle = document.getElementById("title");
+const selectFeedlabel = document.getElementById("feedlabel-select");
 //const data = (STEAM.appnews.newsistem)
 
 
 //Mostrando la Data
 const showData = (data) => {
     let result = '';
-    //console.log(STEAM)
+    //console.log(data)
     data.forEach(element => {
         //element --> DATA[i]
         //console.log(element.house);
-        if (element.author === '' && element.feedlabel !== '') {
+       if (element.author === '' && element.feedlabel !== '') {
             result = containerRoot.innerHTML += `
             <div>
               <div class="card">
                 <div class="box">
-                  <div class="img">
-                      <img src="${element.url}">
+                  <div class="ahref">
+                      <a href="${element.url}">
                   </div>
                   <h2>${element.title}<br><span>Autor: Desconocido</span></h2>
                   <p>FeedLabel: ${element.feedlabel}</p>
@@ -43,7 +43,49 @@ const showData = (data) => {
     return result;
    }
 
+   //Filtrando
+   selectFeedlabel.addEventListener('change', () => {
+    let condition = selectFeedlabel.value
+    let filtered = filterFeedlabel(data, condition);
+    // limpio div
+    containerRoot.innerHTML = '';
+  
+    filtered.forEach(element => {
+        if (element.author === '' && element.feedlabel !== '') {
+            result = containerRoot.innerHTML += `
+            <div>
+              <div class="card">
+                <div class="box">
+                  <div class="ahref">
+                      <a href="${element.url}">
+                  </div>
+                  <h2>${element.title}<br><span>Autor: Desconocido</span></h2>
+                  <p>FeedLabel: ${element.feedlabel}</p>
+                </div>
+              </div>
+            </div>`
+          } else {
+            result = containerRoot.innerHTML += `
+            <div>
+              <div class="card">
+                <div class="box">
+                  <div class="img">
+                      <img src="${element.url}">
+                  </div>
+                  <h2>${element.title}<br><span>${element.author}</span></h2>
+                  <p>FeedLabel: ${element.feedlabel}</p>
+                </div>
+              </div>
+            </div>` 
+          }
+    })
+  })
+
+
+  //Ordenar
+
    window.onload = showData(data);
+
   
 
   /* result = containerRoot.innerHTML += `
