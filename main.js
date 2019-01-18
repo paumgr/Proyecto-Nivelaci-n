ab7485369009c72b918520566cfa18dc76e3f8da
Your Name <you@example.com>
2019-01-18 10:58:43 -0300

    agregando funcion para mostrar tarjetas y modifico filtrar

--- main.js
+++ main.js
@@ -1,90 +1,76 @@
 
-const data = (STEAM.appnews.newsitems);
+
+
+const data = (window.STEAM.appnews.newsitems);
 const containerRoot = document.getElementById("root");
 const selectFeedlabel = document.getElementById("feedlabel-select");
+const orderData = document.getElementById("orderdata")
 //const data = (STEAM.appnews.newsistem)
-
 
 //Mostrando la Data
 const showData = (data) => {
     let result = '';
     //console.log(data)
-    data.forEach(element => {
-        //element --> DATA[i]
-        //console.log(element.house);
-       if (element.author === '' && element.feedlabel !== '') {
-            result = containerRoot.innerHTML += `
-            <div>
-              <div class="card">
-                <div class="box">
-                  <div class="ahref">
-                      <a href="${element.url}">
-                  </div>
-                  <h2>${element.title}<br><span>Autor: Desconocido</span></h2>
-                  <p>FeedLabel: ${element.feedlabel}</p>
-                </div>
-              </div>
-            </div>`
-          } else {
-            result = containerRoot.innerHTML += `
-            <div>
-              <div class="card">
-                <div class="box">
-                  <div class="img">
-                      <img src="${element.url}">
-                  </div>
-                  <h2>${element.title}<br><span>${element.author}</span></h2>
-                  <p>FeedLabel: ${element.feedlabel}</p>
-                </div>
-              </div>
-            </div>` 
-          }
-        });
+    cardSteam(data);
     return result;
-   }
-
+}
+        
+  
    //Filtrando
-   selectFeedlabel.addEventListener('change', () => {
+    selectFeedlabel.addEventListener('change', () => {
     let condition = selectFeedlabel.value
-    let filtered = filterFeedlabel(data, condition);
+    let filtered = window.filterFeedlabel(data, condition);
+    //limpiando mi div
+    containerRoot.innerHTML = '';
+    cardSteam(filtered);
+    })
+
+    //Ordenando
+
+
+  //Función para todas las tarjetas
+  function cardSteam(dataSteam){
     // limpio div
     containerRoot.innerHTML = '';
-  
-    filtered.forEach(element => {
-        if (element.author === '' && element.feedlabel !== '') {
-            result = containerRoot.innerHTML += `
-            <div>
-              <div class="card">
-                <div class="box">
-                  <div class="ahref">
-                      <a href="${element.url}">
-                  </div>
-                  <h2>${element.title}<br><span>Autor: Desconocido</span></h2>
-                  <p>FeedLabel: ${element.feedlabel}</p>
+    dataSteam.forEach(element => {
+      if (element.author === '' && element.feedlabel !== '') {
+          containerRoot.innerHTML += `
+          <div>
+            <div class="card">
+              <div class="box">
+                <div class="ahref">
+                    <a href="${element.url}">
                 </div>
+                <h2>${element.title}<br><span>Autor: Desconocido</span></h2>
+                <p>FeedLabel: ${element.feedlabel}</p>
               </div>
-            </div>`
-          } else {
-            result = containerRoot.innerHTML += `
-            <div>
-              <div class="card">
-                <div class="box">
-                  <div class="img">
-                      <img src="${element.url}">
-                  </div>
-                  <h2>${element.title}<br><span>${element.author}</span></h2>
-                  <p>FeedLabel: ${element.feedlabel}</p>
+            </div>
+          </div>`
+        } else {
+          containerRoot.innerHTML += `
+          <div>
+            <div class="card">
+              <div class="box">
+                <div class="img">
+                    <img src="${element.url}">
                 </div>
+                <h2>${element.title}<br><span>${element.author}</span></h2>
+                <p>FeedLabel: ${element.feedlabel}</p>
               </div>
-            </div>` 
-          }
-    })
-  })
+            </div>
+          </div>` 
+        }
+      });
+ }
 
+  window.onload = showData(data);
+ 
 
   //Ordenar
 
-   window.onload = showData(data);
+  
+
+  
 
   
 
